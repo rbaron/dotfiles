c424af24efad0a4459a521964996719358c12084
Raphael Baron <rbaron@rbaron.net>
2016-04-10 11:22:33 -0300

    - No mo pyenv (having trouble compiling python versions with latest openssl lib on arch linux)
    - Added ipdb macro to vimrc.

--- python/virtualenv.zsh
+++ python/virtualenv.zsh
@@ -1,14 +1,14 @@
 
 # virtualenv  stuff
 export WORKON_HOME=~/.virtualenvs
-VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2
 source /usr/bin/virtualenvwrapper.sh
 
 # pip packages
-PATH=$HOME/.local/bin:$PATH
+#PATH=$HOME/.local/bin:$PATH
 
 # pyenv stuff
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH=$PATH:"$PYENV_ROOT/bin"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH=$PATH:"$PYENV_ROOT/bin"
+#eval "$(pyenv init -)"
+#eval "$(pyenv virtualenv-init -)"
